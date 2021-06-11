c3ae21c3e56bab577fb823fa3f2b3190a81c9ac9
Lukas Eschstruth <lukas.eschstruth@brandung.de>
2021-06-11 12:39:52 +0200

    nice declarative way

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,8 +1,7 @@
 import {HttpClient} from '@angular/common/http';
-import {identifierName} from '@angular/compiler';
 import {Injectable} from '@angular/core';
-import {BehaviorSubject, combineLatest} from 'rxjs';
-import {map, tap} from 'rxjs/operators';
+import {BehaviorSubject, Subject} from 'rxjs';
+import {defaultIfEmpty, shareReplay, startWith, switchMap, tap} from 'rxjs/operators';
 
 export type User = {
   id: number,
@@ -30,8 +29,8 @@ export type User = {
 
 const DEFAULT_USER: User = {
   id: 0,
-  name: '-',
-  username: '-',
+  name: 'default',
+  username: 'username',
   email: ''
 }
 
@@ -39,86 +38,16 @@ const DEFAULT_USER: User = {
   providedIn: 'root'
 })
 export class UserService {
-  private userSubject = new BehaviorSubject<User>(DEFAULT_USER);
-  private selectedUserIdSubject = new BehaviorSubject<number | null>(null);
+  private selectedUserIdSubject = new Subject<number>();
 
-  user$ = combineLatest([this.userSubject.asObservable()]).pipe(
-    map(([user] =>))
+  user$ = this.selectedUserIdSubject.asObservable().pipe(
+    switchMap(id => this.http.get<User>(`https://jsonplaceholder.typicode.com/users/${id}`),
+    ),
+    startWith(DEFAULT_USER),
+    shareReplay(1),
   );
 
   constructor(private http: HttpClient) {
-  }
-
-  loadUser(id: number) {
-    return this.http.get<User>(`https://jsonplaceholder.typicode.com/users/${id}`).pipe(
-      tap(user => this.userSubject.next(user)),
-      map(() => true)
-    );
-  }
-
-  loadRandomUser() {
-    const id = Math.ceil(Math.random() * 10);
-    this.loadUser(id);
-  }
-}
-
-
-
-import {HttpClient} from '@angular/common/http';
-import {identifierName} from '@angular/compiler';
-import {Injectable} from '@angular/core';
-import {BehaviorSubject, combineLatest, Subject} from 'rxjs';
-import {map, takeUntil, tap} from 'rxjs/operators';
-
-export type User = {
-  id: number,
-  name: string,
-  username: string,
-  email: string,
-  address?: {
-    street: string,
-    suite: string,
-    city: string,
-    zipcode: string,
-    geo: {
-      lat: number,
-      lng: number
-    }
-  },
-  phone?: string,
-  website?: string,
-  company?: {
-    name: string,
-    catchPhrase: string,
-    bs: string
-  }
-}
-
-const DEFAULT_USER: User = {
-  id: 0,
-  name: '-',
-  username: '-',
-  email: ''
-}
-
-@Injectable({
-  providedIn: 'root'
-})
-export class UserService {
-  private userSubject = new BehaviorSubject<User>(DEFAULT_USER);
-  private selectedUserIdSubject = new Subject<number>();
-  private serviceDestroyedSubject = new Subject<number>();
-
-  user$ = this.userSubject.asObservable();
-
-  constructor(private http: HttpClient) {
-    this.selectedUserIdSubject.pipe(takeUntil(this.serviceDestroyedSubject)).subscribe(newId => {
-      
-    })
-  }
-
-  ngOnDestroy() {
-    this.serviceDestroyedSubject.next();
   }
 
   loadUser(id: number) {
@@ -130,7 +59,5 @@ export class UserService {
     this.loadUser(id);
   }
 }
-
-
 
 
