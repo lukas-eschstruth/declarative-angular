c4a9a0185f503cce45c69f2f63a2f9b9b5dafc6e
Lukas Eschstruth <lukas.eschstruth@brandung.de>
2021-06-16 09:43:47 +0200

    add change detection push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {Observable, Subject} from 'rxjs';
 import {Post, PostsService} from './services/posts.service';
 import {User, UserService} from './services/user.service';
@@ -6,13 +6,17 @@ import {User, UserService} from './services/user.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
   constructor(private userService: UserService, private postsService: PostsService) {
   }
 
   get user$(): Observable<User> {
+    // a console.log as quick and dirty measure of how often change detection is run
+    // set changeDetection to ChangeDetectionStrategy.Default to see the difference
+    console.log('get user$() called');
     return this.userService.user$;
   }
 
@@ -26,6 +30,11 @@ export class AppComponent {
 
   onLoadUserClicked() {
     this.userService.loadRandomUser();
+  }
+
+  onRandomButtonClicked() {
+    console.log('onRandomButtonClicked');
+
   }
 }
 
